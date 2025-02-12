c774a4c8a577239591feb81b25b71d5aecfbbbef
farelibrahim34 <105421371+farelibrahim34@users.noreply.github.com>
2025-02-12 14:16:19 +0700

    update enkripsi

--- app/src/main/java/com/example/tespraktikum/MenuEnkripsi.kt
+++ app/src/main/java/com/example/tespraktikum/MenuEnkripsi.kt
@@ -5,16 +5,49 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.tespraktikum.databinding.ActivityMenuAlgoritmaBinding
+import com.example.tespraktikum.databinding.ActivityMenuEnkripsiBinding
+import javax.crypto.SecretKey
+import javax.crypto.spec.IvParameterSpec
 
 class MenuEnkripsi : AppCompatActivity() {
+    private lateinit var secretKey: SecretKey
+    private lateinit var iv: IvParameterSpec
+    lateinit var binding : ActivityMenuEnkripsiBinding
+    private var encryptedText = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_menu_enkripsi)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMenuEnkripsiBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        secretKey = AESHelper.generateSecretKey()
+        iv = AESHelper.generateIV()
+
+        binding.buttonEn.setOnClickListener {
+            val textToEncrypt = binding.editTextText3.text.toString().trim()
+
+            if (textToEncrypt.isNotEmpty()) {
+                encryptedText = AESHelper.encrypt(
+                    textToEncrypt = textToEncrypt,
+                    secretKey = secretKey,
+                    iv = iv
+                )
+
+                binding.editTextText3.text = encryptedText
+            }
+
+        }
+
+        binding.buttonDek.setOnClickListener {
+            if (encryptedText.isNotEmpty()) {
+                val decryptedText = AESHelper.decrypt(
+                    encryptedText = encryptedText,
+                    secretKey = secretKey,
+                    iv = iv
+                )
+
+                binding.editTextText4.text = decryptedText
+            }
         }
     }
 }
